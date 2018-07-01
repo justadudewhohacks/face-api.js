157d15e76c7be812f7d93d51d8823b9b095fa207
vincent <muehler.v@gmail.com>
2018-07-01 15:44:43 +0200

    check in latest build

--- build/faceLandmarkNet/FaceLandmarkNet.d.ts
+++ build/faceLandmarkNet/FaceLandmarkNet.d.ts
@@ -6,5 +6,7 @@ export declare class FaceLandmarkNet {
     private _params;
     load(weightsOrUrl: Float32Array | string | undefined): Promise<void>;
     extractWeights(weights: Float32Array): void;
+    forwardTensor(imgTensor: tf.Tensor4D): tf.Tensor2D;
+    forward(input: tf.Tensor | NetInput | TNetInput): Promise<tf.Tensor2D>;
     detectLandmarks(input: tf.Tensor | NetInput | TNetInput): Promise<FaceLandmarks>;
 }
