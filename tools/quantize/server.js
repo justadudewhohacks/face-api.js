a97cd220a13f64f8b7467c1e79643e0774622d0e
vincent <muehler.v@gmail.com>
2018-08-17 08:57:23 +0200

    qunatized tiny_yolov2_separable_conv_model

--- tools/quantize/server.js
+++ tools/quantize/server.js
@@ -9,9 +9,10 @@ app.use(express.static(path.join(__dirname, './public')))
 app.use(express.static(path.join(__dirname, './node_modules/file-saver')))
 app.use(express.static(path.join(__dirname, '../../examples/public')))
 app.use(express.static(path.join(__dirname, '../../weights')))
+app.use(express.static(path.join(__dirname, '../../weights_uncompressed')))
 app.use(express.static(path.join(__dirname, '../../dist')))
 
-app.get('/', (req, res) => res.redirect('/quantize_tiny_yolov2'))
-app.get('/quantize_tiny_yolov2', (req, res) => res.sendFile(path.join(viewsDir, 'quantizeTinyYolov2.html')))
+app.get('/', (req, res) => res.redirect('/quantize_model'))
+app.get('/quantize_model', (req, res) => res.sendFile(path.join(viewsDir, 'quantizeModel.html')))
 
 app.listen(3000, () => console.log('Listening on port 3000!'))
