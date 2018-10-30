1a1b018fec8357deb8fd04b4dccc246791a3d26c
vincent <muehler.v@gmail.com>
2018-10-30 13:40:19 +0100

    check in latest build

--- build/commonjs/faceDetectionNet/loadQuantizedParams.d.ts
+++
@@ -1,6 +0,0 @@
-import { ParamMapping } from 'tfjs-image-recognition-base';
-import { NetParams } from './types';
-export declare function loadQuantizedParams(uri: string | undefined): Promise<{
-    params: NetParams;
-    paramMappings: ParamMapping[];
-}>;
--- build/es6/faceDetectionNet/nonMaxSuppression.d.ts
+++
@@ -1,2 +0,0 @@
-import * as tf from '@tensorflow/tfjs-core';
-export declare function nonMaxSuppression(boxes: tf.Tensor2D, scores: number[], maxOutputSize: number, iouThreshold: number, scoreThreshold: number): number[];
