0c62e7bb34470636f5ccf47880b9715cb99952b1
vincent <muehler.v@gmail.com>
2018-12-13 14:50:14 +0100

    move faceLandmarks training scripts to seperate directory

--- tools/train/faceLandmarks/server.js
+++ tools/train/faceLandmarks/server.js
@@ -1,4 +1,4 @@
-require('./faceLandmarks/.env')
+require('./.env')
 
 const express = require('express')
 const path = require('path')
@@ -6,13 +6,13 @@ const fs = require('fs')
 
 const app = express()
 
-const publicDir = path.join(__dirname, './faceLandmarks')
+const publicDir = path.join(__dirname, './public')
 app.use(express.static(publicDir))
-app.use(express.static(path.join(__dirname, './shared')))
-app.use(express.static(path.join(__dirname, './node_modules/file-saver')))
-app.use(express.static(path.join(__dirname, '../../examples/public')))
-app.use(express.static(path.join(__dirname, '../../weights')))
-app.use(express.static(path.join(__dirname, '../../dist')))
+app.use(express.static(path.join(__dirname, '../shared')))
+app.use(express.static(path.join(__dirname, '../node_modules/file-saver')))
+app.use(express.static(path.join(__dirname, '../../../examples/public')))
+app.use(express.static(path.join(__dirname, '../../../weights')))
+app.use(express.static(path.join(__dirname, '../../../dist')))
 
 const trainDataPath = path.resolve(process.env.TRAIN_DATA_PATH)
 
