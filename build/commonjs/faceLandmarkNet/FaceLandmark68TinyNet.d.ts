affda1dfbb0c299ca0d777d5a7d75ce76934b8b4
vincent <muehler.v@gmail.com>
2019-01-04 12:44:48 +0100

    check in fixed build

--- build/commonjs/faceLandmarkNet/FaceLandmark68TinyNet.d.ts
+++ build/commonjs/faceLandmarkNet/FaceLandmark68TinyNet.d.ts
@@ -1,5 +1,5 @@
-import { TinyFaceFeatureExtractorParams } from 'src/faceFeatureExtractor/types';
 import { TinyFaceFeatureExtractor } from '../faceFeatureExtractor/TinyFaceFeatureExtractor';
+import { TinyFaceFeatureExtractorParams } from '../faceFeatureExtractor/types';
 import { FaceLandmark68NetBase } from './FaceLandmark68NetBase';
 export declare class FaceLandmark68TinyNet extends FaceLandmark68NetBase<TinyFaceFeatureExtractorParams> {
     constructor(faceFeatureExtractor?: TinyFaceFeatureExtractor);
