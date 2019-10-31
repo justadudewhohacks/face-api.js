3b010ccf0f653ca06858c0862f71585eb6f097c4
leer <leer561@gmail.com>
2019-10-31 17:59:53 +0800

    增加图片对比识别

--- examples/examples-browser/public/js/bbt.js
+++ examples/examples-browser/public/js/bbt.js
@@ -1,53 +1,73 @@
 const classes = ['amy', 'bernadette', 'howard', 'leonard', 'penny', 'raj', 'sheldon', 'stuart']
 
 function getFaceImageUri(className, idx) {
-  return `${className}/${className}${idx}.png`
+	return `${className}/${className}${idx}.png`
 }
 
 function renderFaceImageSelectList(selectListId, onChange, initialValue) {
-  const indices = [1, 2, 3, 4, 5]
-  function renderChildren(select) {
-    classes.forEach(className => {
-      const optgroup = document.createElement('optgroup')
-      optgroup.label = className
-      select.appendChild(optgroup)
-      indices.forEach(imageIdx =>
-        renderOption(
-          optgroup,
-          `${className} ${imageIdx}`,
-          getFaceImageUri(className, imageIdx)
-        )
-      )
-    })
-  }
+	const indices = [1, 2, 3, 4, 5]
 
-  renderSelectList(
-    selectListId,
-    onChange,
-    getFaceImageUri(initialValue.className, initialValue.imageIdx),
-    renderChildren
-  )
+	function renderChildren(select) {
+		classes.forEach(className => {
+			const optgroup = document.createElement('optgroup')
+			optgroup.label = className
+			select.appendChild(optgroup)
+			indices.forEach(imageIdx =>
+				renderOption(
+					optgroup,
+					`${className} ${imageIdx}`,
+					getFaceImageUri(className, imageIdx)
+				)
+			)
+		})
+	}
+
+	renderSelectList(
+		selectListId,
+		onChange,
+		getFaceImageUri(initialValue.className, initialValue.imageIdx),
+		renderChildren
+	)
 }
 
 // fetch first image of each class and compute their descriptors
 async function createBbtFaceMatcher(numImagesForTraining = 1) {
-  const maxAvailableImagesPerClass = 5
-  numImagesForTraining = Math.min(numImagesForTraining, maxAvailableImagesPerClass)
+	const maxAvailableImagesPerClass = 5
+	numImagesForTraining = Math.min(numImagesForTraining, maxAvailableImagesPerClass)
 
-  const labeledFaceDescriptors = await Promise.all(classes.map(
-    async className => {
-      const descriptors = []
-      for (let i = 1; i < (numImagesForTraining + 1); i++) {
-        const img = await faceapi.fetchImage(getFaceImageUri(className, i))
-        descriptors.push(await faceapi.computeFaceDescriptor(img))
-      }
+	const labeledFaceDescriptors = await Promise.all(classes.map(
+		async className => {
+			const descriptors = []
+			for (let i = 1; i < (numImagesForTraining + 1); i++) {
+				const img = await faceapi.fetchImage(getFaceImageUri(className, i))
+				descriptors.push(await faceapi.computeFaceDescriptor(img))
+			}
 
-      return new faceapi.LabeledFaceDescriptors(
-        className,
-        descriptors
-      )
-    }
-  ))
+			return new faceapi.LabeledFaceDescriptors(
+				className,
+				descriptors
+			)
+		}
+	))
 
-  return new faceapi.FaceMatcher(labeledFaceDescriptors)
+	return new faceapi.FaceMatcher(labeledFaceDescriptors)
+}
+
+// 获取文件名以及设置对比模型
+async function createBsmFaceMatcher() {
+	const list = await $.get("/imageList")
+	return Promise.all(list.map(async file => {
+		// fetch image data from urls and convert blob to HTMLImage element
+		const imgUrl = `/bbt/${file}`
+		const img = await faceapi.fetchImage(imgUrl)
+
+		// detect the face with the highest score in the image and compute it's landmarks and face descriptor
+		const fullFaceDescription = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+
+		if (!fullFaceDescription) {
+			throw new Error(`no faces detected for ${file}`)
+		}
+		const faceDescriptors = [fullFaceDescription.descriptor]
+		return new faceapi.LabeledFaceDescriptors(file.split('.')[0], faceDescriptors)
+	}))
 }
--- examples/examples-browser/server.js
+++ examples/examples-browser/server.js
@@ -1,11 +1,18 @@
+
 const express = require('express')
 const path = require('path')
-const { get } = require('request')
+const {get} = require('request')
+const fs = require('fs') //引用文件系统模块
+
+// 读取本地文件夹图片
+console.log('开始读取本地图片并确定识别数据!')
+let images = []
+fs.readdir('./public/bbt', (err, files) =>images = files)
 
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({extended: true}))
 
 const viewsDir = path.join(__dirname, 'views')
 app.use(express.static(viewsDir))
@@ -16,7 +23,7 @@ app.use(express.static(path.join(__dirname, '../../weights')))
 app.use(express.static(path.join(__dirname, '../../dist')))
 
 app.get('/', (req, res) => res.redirect('/face_detection'))
-app.get('/face_detection',(req, res) => res.sendFile(path.join(viewsDir, 'webcamFaceExpressionRecognition.html')))
+app.get('/face_detection', (req, res) => res.sendFile(path.join(viewsDir, 'webcamFaceExpressionRecognition.html')))
 app.get('/face_landmark_detection', (req, res) => res.sendFile(path.join(viewsDir, 'faceLandmarkDetection.html')))
 app.get('/face_expression_recognition', (req, res) => res.sendFile(path.join(viewsDir, 'faceExpressionRecognition.html')))
 app.get('/age_and_gender_recognition', (req, res) => res.sendFile(path.join(viewsDir, 'ageAndGenderRecognition.html')))
@@ -35,39 +42,44 @@ app.get('/batch_face_landmarks', (req, res) => res.sendFile(path.join(viewsDir, 
 app.get('/batch_face_recognition', (req, res) => res.sendFile(path.join(viewsDir, 'batchFaceRecognition.html')))
 
 app.post('/fetch_external_image', async (req, res) => {
-  const { imageUrl } = req.body
-  if (!imageUrl) {
-    return res.status(400).send('imageUrl param required')
-  }
-  try {
-    const externalResponse = await request(imageUrl)
-    res.set('content-type', externalResponse.headers['content-type'])
-    return res.status(202).send(Buffer.from(externalResponse.body))
-  } catch (err) {
-    return res.status(404).send(err.toString())
-  }
+	const {imageUrl} = req.body
+	if (!imageUrl) {
+		return res.status(400).send('imageUrl param required')
+	}
+	try {
+		const externalResponse = await request(imageUrl)
+		res.set('content-type', externalResponse.headers['content-type'])
+		return res.status(202).send(Buffer.from(externalResponse.body))
+	} catch (err) {
+		return res.status(404).send(err.toString())
+	}
+})
+
+// 响应图片列表
+app.get('/imageList', async (req, res) => {
+	res.status(200).json(images);
 })
 
 app.listen(3000, () => console.log('Listening on port 3000!'))
 
 function request(url, returnBuffer = true, timeout = 10000) {
-  return new Promise(function(resolve, reject) {
-    const options = Object.assign(
-      {},
-      {
-        url,
-        isBuffer: true,
-        timeout,
-        headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
-        }
-      },
-      returnBuffer ? { encoding: null } : {}
-    )
+	return new Promise(function (resolve, reject) {
+		const options = Object.assign(
+			{},
+			{
+				url,
+				isBuffer: true,
+				timeout,
+				headers: {
+					'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+				}
+			},
+			returnBuffer ? {encoding: null} : {}
+		)
 
-    get(options, function(err, res) {
-      if (err) return reject(err)
-      return resolve(res)
-    })
-  })
+		get(options, function (err, res) {
+			if (err) return reject(err)
+			return resolve(res)
+		})
+	})
 }
