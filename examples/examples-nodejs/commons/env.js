765dd78130a677b38cece0a6153c4de5ca60ca7f
Joe <appdev@kreolact.com>
2024-01-24 05:45:21 +0000

    Old School

---
+++ examples/examples-nodejs/commons/env.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+// import nodejs bindings to native tensorflow,
+// not required, but will speed up things drastically (python required)
+require("@tensorflow/tfjs-node");
+var faceapi = require("face-api.js");
+// implements nodejs wrappers for HTMLCanvasElement, HTMLImageElement, ImageData
+var canvas = require('canvas');
+exports.canvas = canvas;
+// patch nodejs environment, we need to provide an implementation of
+// HTMLCanvasElement and HTMLImageElement
+var Canvas = canvas.Canvas, Image = canvas.Image, ImageData = canvas.ImageData;
+faceapi.env.monkeyPatch({ Canvas: Canvas, Image: Image, ImageData: ImageData });
---
+++ examples/examples-nodejs/commons/faceDetection.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+var faceapi = require("face-api.js");
+exports.faceDetectionNet = faceapi.nets.ssdMobilenetv1;
+// export const faceDetectionNet = tinyFaceDetector
+// SsdMobilenetv1Options
+var minConfidence = 0.5;
+// TinyFaceDetectorOptions
+var inputSize = 408;
+var scoreThreshold = 0.5;
+function getFaceDetectorOptions(net) {
+    return net === faceapi.nets.ssdMobilenetv1
+        ? new faceapi.SsdMobilenetv1Options({ minConfidence: minConfidence })
+        : new faceapi.TinyFaceDetectorOptions({ inputSize: inputSize, scoreThreshold: scoreThreshold });
+}
+exports.faceDetectionOptions = getFaceDetectorOptions(exports.faceDetectionNet);
